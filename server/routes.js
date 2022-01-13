31aa2ad40384d3794e8e0699ba910a466af644d0
Trevor <trevorhackreactor@Gmail.com>
2022-01-13 08:03:08 -0500

    loader.io verification

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@
 // eslint-disable-next-line import/extensions
 
 const express = require('express');
+const path = require('path');
 
 // eslint-disable-next-line import/extensions
 const {
@@ -19,6 +20,20 @@ const {
 const app = express();
 app.use(express.static('Public'));
 
+app.get("/loaderio-13f9b0a968c3dfa8d978a7d7d389d4f9.txt", (req, res, next) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+
+  const fileName = "loaderio-13f9b0a968c3dfa8d978a7d7d389d4f9.txt";
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
 app.get('/products', (req, res) => {
   getProductsWithSupressedFeatures()
     .then((products) => {
